ae8205a38df93d007d1d2c74f72d7d7554c37d02
zhouming <minerchow@126.com>
2019-06-09 10:51:52 +0800

    update

--- class.ts
+++ class.ts
@@ -43,15 +43,15 @@ console.log(c.say());
 // }
 
 
-// class Boss extends Person {
+// class Coder extends Person {
 //     say():string{
 //      //   return this.age
-//         return super.say() + ",是个大老板";
+//         return super.say() + ",是个小前端";
 //     }
 // }
 // let p = new Person("xiaoming",18);
 // console.log(p.say());
 // p.Age = 10;
 // console.log(p.Age);
-// let b = new Boss("桂总",18);
+// let b = new Coder("xiaoming",18);
 // console.log(b.say());
